1904ef31200dab259ae355fc0b4272272afdfbc7
Matt Berns <matthewl.berns@gmail.com>
2014-06-15 14:36:59 -0500

    Built out tabs and updated image files

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
 $(function() {
-  $( "#skill_tabs" ).tabs();
+  $("#languages").attr("class", "active_tab")
+  $("#skill_list li").click(function(event){
+    changeTab($(this).attr("name"));
+  })
 });
+
+function changeTab(new_tab) {
+  $("#skill_display_box").find("div").attr("class", "active_tab").removeClass("active_tab")
+  $("#" + new_tab).addClass("active_tab")
+}
