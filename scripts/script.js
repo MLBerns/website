8901c74edfbfe3c40969246c6197f25c70faf0d0
Matt Berns <matthewl.berns@gmail.com>
2014-06-15 18:10:55 -0500

    Completed MVP of site, focusing on formatting

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,17 @@
 $(function() {
-  $("#languages").attr("class", "active_tab")
+  $("#languages").css("display", "block").attr("name", "active_tab")
   $("#skill_list li").click(function(event){
     changeTab($(this).attr("name"));
-  })
+  });
+  $("#d3svg").css("height", 175)
 });
 
 function changeTab(new_tab) {
-  $("#skill_display_box").find("div").attr("class", "active_tab").removeClass("active_tab")
-  $("#" + new_tab).addClass("active_tab")
+  if ( $("#" + new_tab).attr("name") == "active_tab" ) {
+    return
+  }
+  $($("#skill_display_box").find("div[name='active_tab']")[0]).css("display", "none").attr("name", "")
+  $("#" + new_tab).toggle("slide", {direction:'left'} ).attr("name", "active_tab")
 }
+
+
