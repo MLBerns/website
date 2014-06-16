5a5b09ca558366f63a63755a6c017187b92cad4b
Matt Berns <matthewl.berns@gmail.com>
2014-06-16 11:47:30 -0500

    Updated script to dynamically change slide direction on skills list

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,29 @@
 $(function() {
   $("#skill_list li[name='languages']").addClass("selected")
   $("#languages").css("display", "block").attr("name", "active_tab")
+  $("#d3svg").css("height", 175)
   $("#skill_list li").click(function(event){
     changeTab($(this).attr("name"));
   });
-  $("#d3svg").css("height", 175)
 });
 
 function changeTab(new_tab) {
   if ( $("#" + new_tab).attr("name") == "active_tab" ) {
     return
   }
+  var old_tab = $("#skill_display_box").find("div[name='active_tab']")[0]
   $("#skill_list li").removeClass("selected")
   $("#skill_list li[name='" + new_tab + "']").addClass("selected")
-  $($("#skill_display_box").find("div[name='active_tab']")[0]).css("display", "none").attr("name", "")
-  $("#" + new_tab).toggle("slide", {direction:'left'} ).attr("name", "active_tab")
+  $(old_tab).css("display", "none").attr("name", "")
+  movement_direction = moveDirection(old_tab, new_tab)
+  $("#" + new_tab).toggle("slide", {direction: movement_direction} ).attr("name", "active_tab")
 }
 
-
+function moveDirection(old_tab, new_tab) {
+  if ( old_tab.compareDocumentPosition(document.getElementById(new_tab)) & Node.DOCUMENT_POSITION_FOLLOWING ) {
+    return "right"
+  }
+  else {
+    return "left"
+  }
+}
