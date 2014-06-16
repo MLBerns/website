db89d79f0e44c51bc0057bafe98825a2400699d9
Matt Berns <matthewl.berns@gmail.com>
2014-06-15 22:44:23 -0500

    Updated tab formatting and image padding

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 $(function() {
+  $("#skill_list li[name='languages']").addClass("selected")
   $("#languages").css("display", "block").attr("name", "active_tab")
   $("#skill_list li").click(function(event){
     changeTab($(this).attr("name"));
@@ -10,6 +11,8 @@ function changeTab(new_tab) {
   if ( $("#" + new_tab).attr("name") == "active_tab" ) {
     return
   }
+  $("#skill_list li").removeClass("selected")
+  $("#skill_list li[name='" + new_tab + "']").addClass("selected")
   $($("#skill_display_box").find("div[name='active_tab']")[0]).css("display", "none").attr("name", "")
   $("#" + new_tab).toggle("slide", {direction:'left'} ).attr("name", "active_tab")
 }
